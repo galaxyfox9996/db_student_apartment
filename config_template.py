d98d956507d06db54c0a659a7f11b46b4b3d0440
galaxyfox9996 <galaxyfox9996@gmail.com>
2026-05-21 15:34:13 +0800

    config

---
+++ config_template.py
@@ -0,0 +1,9 @@
+DB_TYPE = "mysql"
+
+MYSQL_HOST = "127.0.0.1"
+MYSQL_PORT = 3306
+MYSQL_USER = "your_username"
+MYSQL_PASSWORD = "your_password"
+MYSQL_DATABASE = "student_apartment"
+
+SECRET_KEY = "replace_with_your_secret_key"
